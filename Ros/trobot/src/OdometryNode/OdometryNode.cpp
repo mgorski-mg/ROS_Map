8ade60b4ba2bad2f58797b79a143c8df63fb59f2
Michał Górski <mgorski@future-processing.com>
2014-09-30 02:51:45 +0200

    change to old odometry

--- Ros/trobot/src/OdometryNode/OdometryNode.cpp
+++ Ros/trobot/src/OdometryNode/OdometryNode.cpp
@@ -18,8 +18,7 @@ double axialDistance;
 double alpha;
 double wheelRadius;
 
-void getOdometry(const trobot::Odometry::ConstPtr& msg)
-{
+void getOdometry(const trobot::Odometry::ConstPtr& msg) {
   ROS_INFO("I heard rWheel: %f", msg->rightWheelSpeed);
   ROS_INFO("I heard lWheel: %f", msg->leftWheelSpeed);
 
@@ -27,12 +26,11 @@ void getOdometry(const trobot::Odometry::ConstPtr& msg)
   vth = (4 * alpha * (msg->rightWheelSpeed - vx)) / (2 * wheelDistance + axialDistance);
 }
 
-void getEncoderCount(const trobot::Encoder::ConstPtr& msg)
-{
+void getEncoderCount(const trobot::Encoder::ConstPtr& msg) {
   ROS_INFO("I heard rWheelCount: %f", msg->rightWheelCount);
   ROS_INFO("I heard lWheelCount: %f", msg->leftWheelCount);
 
-  if(msg->rightWheelCount > 0 && msg->rightWheelCount > 0){
+  if(msg->rightWheelCount > 0 && msg->rightWheelCount > 0) {
     double R = -msg->rightWheelCount * axialDistance / (msg->leftWheelCount - msg->rightWheelCount);
     double a = 4.8 * msg->leftWheelCount * wheelRadius / R;
     double beta = 90 - a / 2;
@@ -40,17 +38,14 @@ void getEncoderCount(const trobot::Encoder::ConstPtr& msg)
     sx = 2 * R * cos(beta) * sin(beta);
     sy = 2 * R * cos(beta) * cos(beta);
     sth = a * M_PI / 180;
-  } 
-  else 
-  {
+  } else {
     sx = 0;
     sy = 0;
     sth = 0; 
   }
 }
 
-void setupParameters()
-{
+void setupParameters() {
   ros::NodeHandle n("~");
 
   n.param("wheel_distance", wheelDistance, 1.0);
@@ -59,8 +54,7 @@ void setupParameters()
   n.param("wheel_radius", wheelRadius, 0.063);
 }
 
-nav_msgs::Odometry createOdomMsg(double x, double y, double th, ros::Time current_time)
-{
+nav_msgs::Odometry createOdomMsg(double x, double y, double th, ros::Time current_time) {
   //since all odometry is 6DOF we'll need a quaternion created from yaw
   geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
 
@@ -104,54 +98,12 @@ int main(int argc, char** argv){
   last_time = ros::Time::now();
 
   ros::Rate r(10);
-  while(n.ok()){
-
+  while(n.ok()) {
     ros::spinOnce();
     current_time = ros::Time::now();
-
-    //compute odometry in a typical way given the velocities of the robot
-    //double dt = (current_time - last_time).toSec();
-    //double delta_x = (vx * cos(th) - vy * sin(th)) * dt;
-    //double delta_y = (vx * sin(th) + vy * cos(th)) * dt;
-    //double delta_th = vth * dt;
-
-    //x += delta_x;
-    //y += delta_y;
-    //th += delta_th;
-
-    //since all odometry is 6DOF we'll need a quaternion created from yaw
-    //geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
-
-    //next, we'll publish the odometry message over ROS
-    //nav_msgs::Odometry odom;
-    //odom.header.stamp = current_time;
-    //odom.header.frame_id = "odom";
-
-    //set the position
-    //odom.pose.pose.position.x = x;
-    //odom.pose.pose.position.y = y;
-    //odom.pose.pose.position.z = 0.0;
-    //odom.pose.pose.orientation = odom_quat;
-
-    //odom.pose.covariance[0] = 0.00001;
-    //odom.pose.covariance[7] = 0.00001;
-    //odom.pose.covariance[14] = 1000000000000.0;
-    //odom.pose.covariance[21] = 1000000000000.0;
-    //odom.pose.covariance[28] = 1000000000000.0;
-    //odom.pose.covariance[35] = 0.001;
-
-    //set the velocity
-    //odom.child_frame_id = "base_footprint";
-    //odom.twist.twist.linear.x = vx;
-    //odom.twist.twist.linear.y = vy;
-    //odom.twist.twist.angular.z = vth;
-
-    //odom.twist.covariance = odom.pose.covariance;
-
-    //publish the message
-    //odom_pub.publish(odom);
-    //odom_pub.publish(createOdomMsg(x, y, th, current_time));
-    odom_pub.publish(createOdomMsg(sx, sy, sth, current_time));
+	
+    odom_pub.publish(createOdomMsg(x, y, th, current_time));
+    //odom_pub.publish(createOdomMsg(sx, sy, sth, current_time));
 
     last_time = current_time;
     r.sleep();
